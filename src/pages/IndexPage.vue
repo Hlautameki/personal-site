98339263357c46c49f0cb50d69f0e405b2f6fe09
mateuszmistecki <mateusz.mistecki@gmail.com>
2024-06-23 00:38:26 +0200

    Move avatar to card

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,18 +1,26 @@
 <template>
   <q-page class="flex flex-center bg-black" bg-color="black">
-    <q-avatar
-      size="150px"
-    >
-      <q-img
-        src="IMG_1365.jpeg"
-      />
-    </q-avatar>
+
     <img
       v-if="false"
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
+    <q-card class="bg-black text-white">
+      <q-card-section class="flex justify-center">
+        <q-avatar
+          size="150px"
+        >
+          <q-img
+            src="IMG_1365.jpeg"
+          />
+        </q-avatar>
+      </q-card-section>
+      <q-card-section>
+        <div class="text-h2 text-weight-medium">Hi, I'm Mateusz</div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
