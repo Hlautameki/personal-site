6a4afa4af3466f233fe84d8d25e7f649ec9e6890
mateuszmistecki <mateusz.mistecki@gmail.com>
2024-06-22 12:19:47 +0200

    Add q-avatar with q-img and add photo to the project

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,12 @@
 <template>
   <q-page class="flex flex-center bg-black" bg-color="black">
+    <q-avatar
+      size="150px"
+    >
+      <q-img
+        src="IMG_1365.jpeg"
+      />
+    </q-avatar>
     <img
       v-if="false"
       alt="Quasar logo"
