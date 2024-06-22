3d1bd20ae0b820aa26d8077d1b188de6878aeeef
mateuszmistecki <mateusz.mistecki@gmail.com>
2024-06-22 11:59:15 +0200

    Hide quasar image in IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,7 @@
 <template>
   <q-page class="flex flex-center">
     <img
+      v-if="false"
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
